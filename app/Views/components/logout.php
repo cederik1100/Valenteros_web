2190eb83aec8f263c7b0346cce0fbb8aa05c5c0c
cederik1100 <156330366+cederik1100@users.noreply.github.com>
2024-11-04 14:33:24 +0800

    Delete app/Views/components/logout.php

--- app/Views/components/logout.php
+++
@@ -1,3 +0,0 @@
-<a href="<?= base_url('auth/logout')?>" class="inline-block align-baseline font-bold bg-gray-300 py-2 px-4 rounded text-gray-800 hover:bg-gray-500 py-2 px-4 rounded">
-    Logout
-</a>
